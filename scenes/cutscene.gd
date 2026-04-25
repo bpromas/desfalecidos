e2da331cd7691cbd2008777e561e346d8041e7ef
D7004 <danicraftbr@hotmail.com>
2026-04-25 11:24:14 -0300

    adicionar dialogic

---
+++ scenes/cutscene.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
+	Dialogic.start("intro")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_timeline_ended():
+	print("dialogue ended")
+	get_tree().change_scene_to_file("res://scenes/rhythm_game.tscn")
+	
+func _on_dialogic_signal(argument:String):
+	print("signal")
