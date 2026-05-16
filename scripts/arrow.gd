f435ca9e8dbbef784b69d20946dbe9464a2c5a51
bpromas <bpromas@gmail.com>
2026-05-16 11:48:20 -0300

    cores e fadeout nas setinhas

--- scripts/arrow.gd
+++ scripts/arrow.gd
@@ -4,11 +4,12 @@ var direction
 var target_time
 var speed = 256
 var time_alive = 0
+var hittable = true
 
 signal missed
 
 @onready var label: Label = $Label
-@onready var color_rect: ColorRect = $ColorRect
+@onready var sprite: Sprite2D = $Sprite2D
 
 var direction_vector = {
 	"up": Vector2.UP,
@@ -17,27 +18,35 @@ var direction_vector = {
 	"left": Vector2.LEFT,
 }
 
-func get_rotation_for_direction(dir):
-	match dir:
-		"up":
-			return 0
-		"right":
-			return deg_to_rad(90)
-		"down":
-			return deg_to_rad(180)
-		"left":
-			return deg_to_rad(270)
+var rotation_direction = {
+	"up": deg_to_rad(0),
+	"right": deg_to_rad(90),
+	"down": deg_to_rad(180),
+	"left": deg_to_rad(270),
+}
+
+var texture_direction = {
+	"up": 'setinha cima',
+	"right": 'setinha direita',
+	"down": 'setinha baixo',
+	"left": 'setinha esquerda',
+}
 
 func hit():
-	queue_free()
+	if(!hittable): return
+	hittable = false
+	
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.5, 1.5), 0.15)
+	tween.parallel().tween_property(self, "modulate:a", 0.0, 0.15)
+	tween.tween_callback(queue_free)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	rotation = get_rotation_for_direction(direction)
-	var c = color_rect.color
-	color_rect.color = Color.from_hsv(rotation, c.s, c.v, c.a)
+	#rotation = rotation_direction[direction] 
+	sprite.texture = load('res://assets/setinhas/'+texture_direction[direction]+'.png')
+	sprite.scale = Vector2(0.05, 0.05)
+	#sprite.modulate = Color.from_hsv(rotation, 1.0, 1.0, 1.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direction_vector[direction] * speed * delta
 	
